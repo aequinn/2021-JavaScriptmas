27b51d1aeb85bd74be8bbeb6022ab9523ae4074d
Anthony Quinn <quinn.ant@gmail.com>
2021-12-29 22:28:27 -0500

    day 6 challenge

---
+++ Day 6/index.js
@@ -0,0 +1,17 @@
+const greeting = document.getElementById("greeting")
+const christmassifierBtn = document.getElementById("christmassifierBtn")
+christmassifierBtn.addEventListener("click", christmassifyName)
+
+function christmassifyName() {
+  
+  greeting.classList.add('christmassified');
+  setTimeout(function(){greeting.classList.remove('christmassified')},2000)
+   // Task:
+  // - Add christmassify class to greeting.
+  //- Check whether christmassifierBtn has christmassify on it. If so, change text to "De-christmassify", if not, change text to "Christmassify"
+}
+
+// Stretch goals:
+// - Play Christmas music when Christmas class is added.
+// - Remove the Christmas class after a given time.
+
